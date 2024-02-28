8ec0708b323537328a888d21656fb0cf82bd4302
AizenTriesHisBestToLearnJava <aizencuan@gmail.com>
2024-02-28 14:07:35 -0800

    the web crawler doesnt work

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,164 +1,244 @@
 package org.example;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+
+
 import java.io.IOException;
-import java.util.Objects;
+import java.util.ArrayList;
 
 public class Product {
-    
+
     private String name;
-    private double price;
-    
-    private final String url;
-    
+    private double price = -1;
+
+    private String url;
+
     private String img;
-    
+
     private String domain;
-    
+
+    private final String amazonPriceHolder = ".priceToPay.reinventPricePriceToPayMargin.aok-align-center.a-price";
+    private final String amazonPriceElement = "span:nth-of-type(2)";
+
+    private final String amazonNameHolder = ".product-title-word-break.a-size-large";
+    private final String ebayPriceHolder = ".x-price-primary";
+    private final String ebayPriceElement = ".ux-textspans";
+
+    private final String ebayNameHolder = ".x-item-title__mainTitle";
+    private final String ebayNameElement = ".ux-textspans--BOLD.ux-textspans";
+
+
     public Product(String url) {
         this.url = url;
         scrapeProduct(url);
     }
-    
+
     public String toString() {
         return "========================\nProduct URL: " + url + "\nProduct image: " + img + "\nProduct's Marketplace: " + domain + "\nProduct Name: " + name + "\nPrice: " + price;
     }
+
     public double getPrice() {
         return price;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public String getUrl() {
         return url;
     }
-    
+
     public String getImg() {
         return img;
     }
+
     public void scrapeProduct(String url) {
-        
+
+        String productURL = url;
+
         String subPriceHolder = "";
         String subPriceElement = "";
         String subNameHolder = "";
         String subNameElement = "";
-        
+
+
         boolean isAmazon = false;
         boolean isEbay = false;
-        
+
         if (this.getProductWebsite(url).equals("www.amazon.com")) {
             isAmazon = true;
-            subPriceHolder = ".priceToPay.reinventPricePriceToPayMargin.aok-align-center.a-price";
-            subPriceElement = "span:nth-of-type(2)";
-            subNameHolder = ".product-title-word-break.a-size-large";
+            subPriceHolder = amazonPriceHolder;
+            subPriceElement = amazonPriceElement;
+            subNameHolder = amazonNameHolder;
         } else if (this.getProductWebsite(url).equals("www.ebay.com")) {
             isEbay = true;
-            subPriceHolder = ".x-price-primary";
-            subPriceElement = ".ux-textspans";
-            subNameHolder = ".x-item-title__mainTitle";
-            subNameElement = ".ux-textspans--BOLD.ux-textspans";
+            subPriceHolder = ebayPriceHolder;
+            subPriceElement = ebayPriceElement;
+            subNameHolder = ebayNameHolder;
+            subNameElement = ebayNameElement;
         }
-        
+
         try {
-            Document document = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows; Windows NT 10.5;) Gecko/20100101 Firefox/65.6").get();
-            
+            Document document = Jsoup.connect(productURL).userAgent(RandomUserAgent.randUserAgent()).get();
+
+            Elements itemsHolder = document.select(".recs-item-list-padding.recs-col-items-1.recs-item-list-container");
+            System.out.println(itemsHolder.outerHtml());
+
+
             Elements priceHolder = document.select(subPriceHolder);
             String priceElement = priceHolder.select(subPriceElement).text();
-            
+            System.out.println(priceHolder.select(subPriceElement).outerHtml());
             Elements nameHolder = document.select(subNameHolder);
-            
+
+            ArrayList<String> urls = new ArrayList<>();
+
+            // Elements urlsHolder = document.getElementsByTag("a");
+            //  for (Element link : urlsHolder) {
+            //     if (!urls.contains(link.attr("abs:href"))) {
+            //         urls.add(link.attr("abs:href"));
+            //     }
+            //     System.out.println(link.attr("abs:href"));
+            //  }
+
+
+            String nameElement = "";
+
             if (isAmazon) {
+
+                if (priceElement.equals("")) {
+                    Elements newPriceHolder = document.select(".apexPriceToPay.a-size-medium.a-text-price.a-price");
+                    priceElement = newPriceHolder.select("span:nth-of-type(2)").text();
+                }
+                nameElement = String.valueOf(nameHolder);
+
                 Element imageHolder = document.getElementById("imgTagWrapperId");
-                
-                assert imageHolder != null;
                 Elements imageTag = imageHolder.getElementsByTag("img");
                 img = imageTag.attr("src");
                 name = imageTag.attr("alt");
-                
+
+
                 this.setAmazonElements(priceElement);
             } else if (isEbay) {
-                String nameElement = nameHolder.select(subNameElement).text();
-                
+                nameElement = nameHolder.select(subNameElement).text();
+
                 Elements imageHolder = document.select(".image.active.image-treatment.ux-image-carousel-item");
-                Elements imageTag = Objects.requireNonNull(imageHolder.first()).getElementsByTag("img");
-                
+                Elements imageTag = imageHolder.first().getElementsByTag("img");
+
                 img = imageTag.attr("src");
-                
+
+
                 this.setEbayElements(priceElement, nameElement);
             }
-            
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     public String getProductWebsite(String url) {
-        StringBuilder domain2 = new StringBuilder();
+        String domain = "";
         int slashCount = 0;
         for (int i = 0; i < url.length(); i++) {
-            if (url.charAt(i) == '/') {
+            if (url.substring(i, i + 1).equals("/")) {
                 if (slashCount == 2) {
                     break;
                 }
                 slashCount++;
-                domain2.append(url.charAt(i));
+                domain += url.substring(i, i + 1);
                 continue;
             }
-            
+
             if (slashCount == 2) {
-                domain2.append(url.charAt(i));
+                domain += url.substring(i, i + 1);
             }
-            
+
+
         }
-        
-        switch (domain2.toString()) {
+
+
+        switch (domain) {
             case "//www.amazon.com":
                 this.domain = "www.amazon.com";
                 return "www.amazon.com";
             case "//www.ebay.com":
                 this.domain = "www.ebay.com";
                 return "www.ebay.com";
-            default:
-                return "What are you even doing?";
         }
+        return null;
     }
-    
+
     public void setAmazonElements(String priceElement) {
-        StringBuilder finalPrice = new StringBuilder();
+
+        String finalPrice = "";
         for (int i = 0; i < priceElement.length(); i++) {
-            if (priceElement.charAt(i) == '$') {
+            if (priceElement.substring(i, i + 1).equals("$")) {
                 continue;
             }
-            if (priceElement.charAt(i) == ' ') {
+            if (priceElement.substring(i, i + 1).equals(" ")) {
                 break;
             }
-            finalPrice.append(priceElement.charAt(i));
+            finalPrice += priceElement.substring(i, i + 1);
         }
-        
-        price = Double.parseDouble(finalPrice.toString());
+
+        System.out.println(finalPrice);
+
+        price = Double.parseDouble(finalPrice);
     }
-    
+
     public void setEbayElements(String priceElement, String nameElement) {
-        StringBuilder finalPrice = new StringBuilder();
+        String finalPrice = "";
         boolean isPrice = false;
         for (int i = 0; i < priceElement.length(); i++) {
-            
-            if (priceElement.charAt(i) == '$') {
+
+            if (priceElement.substring(i, i + 1).equals("$")) {
                 isPrice = true;
                 continue;
             }
             if (isPrice) {
-                finalPrice.append(priceElement.charAt(i));
+                finalPrice += priceElement.substring(i, i + 1);
             }
         }
-        
+
+
         name = nameElement;
-        price = Double.parseDouble(finalPrice.toString());
+        price = Double.parseDouble(finalPrice);
     }
+
+    public ArrayList<String> findProduct(String url, int depth, boolean isAmazon) throws IOException, InterruptedException {
+
+        ArrayList<String> links = new ArrayList<>();
+        if (depth == 0) {
+            return null;
+        }
+
+        Document doc = Jsoup.connect(url).userAgent(RandomUserAgent.randUserAgent()).get();
+        if (!isAmazon) {
+            Elements itemsHolder = doc.select(".recs-item-list-padding.recs-col-items-1.recs-item-list-container");
+            System.out.println(itemsHolder.html());
+            for (Element item : itemsHolder){
+                String realURL = item.attr("abs:href");
+
+                if (realURL != ""){
+                    Product temp = new Product(realURL);
+                    temp.scrapeProduct(realURL);
+
+                    if (temp.getPrice()<this.getPrice()){
+                        links.add(temp.getUrl());
+                        findProduct(realURL,depth--, isAmazon);
+                    }
+                }
+
+
+            }
+        }
+
+        return links;
+    }
+
+
 }
